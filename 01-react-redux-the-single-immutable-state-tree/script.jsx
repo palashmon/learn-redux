8ad4387395453466693a795c581834450345b2a5
Palash Mondal <palashm@live.in>
2018-05-02 07:21:52 +0530

    Improve format/structure :art:

--- 01-react-redux-the-single-immutable-state-tree/script.jsx
+++ 01-react-redux-the-single-immutable-state-tree/script.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 export default class componentName extends Component {
-    render() {
-        return <div />;
-    }
+  render() {
+    return <div />;
+  }
 }
